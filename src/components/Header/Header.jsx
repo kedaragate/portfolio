9d51eda6367d9bb1d1fb15ee3e84c290e7f9321a
Kedar Agate <kedar.agate@gmail.com>
2022-09-13 09:00:03 +0530

    greet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import "./Header.css";
 
 export default function Header() {
+  function getCurrentTime() {
+    let currentTime = new Date().getHours();
+    if (currentTime > 5 && currentTime < 12) {
+      return "Good Morning!";
+    } else if ((currentTime >= 12) & (currentTime < 5)) {
+      return "Good Afternoon!";
+    } else {
+      return "Good Evening!";
+    }
+  }
+
   return (
     <div className="header-section">
-      <span className="heyThereText heading">Hey, there I am</span>
-      <span className="myName heading">{`Kedar Agate |`}</span>
+      <span className="heyThereText heading">
+        Hey, there {getCurrentTime()}
+      </span>
+      <span className="myName heading">{`I am Kedar Agate |`}</span>
       <span className="webDeveloperText heading">{`<WebDeveloper/>`}</span>
       <hr></hr>
     </div>
